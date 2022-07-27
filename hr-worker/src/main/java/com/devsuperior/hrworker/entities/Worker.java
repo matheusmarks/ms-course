fc527a1c0b08dee92562c71a0c2b26e6473c86e4
matheusmarks <marquesmatheusoliveira01@gmail.com>
2022-07-26 22:33:07 -0300

    Re-creating hr-worker project

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,67 @@
+package com.devsuperior.hrworker.entities;
+
+import org.springframework.context.annotation.EnableMBeanExport;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Double dailyIncome;
+
+    public Worker() {}
+
+    public Worker(Long id, String nome, Double dailyIncome) {
+        this.id = id;
+        this.nome = nome;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return id.equals(worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
