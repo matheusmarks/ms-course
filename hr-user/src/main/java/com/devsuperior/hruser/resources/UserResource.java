15a0450d84c8b94220f078b34ef49a415462f558
matheusmarks <marquesmatheusoliveira01@gmail.com>
2022-08-14 11:03:18 -0300

    UserRepository, UserResource, Zuul Config

---
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -0,0 +1,36 @@
+package com.devsuperior.hruser.resources;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RefreshScope
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userRepository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User obj = userRepository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+}
+
