722b20ef53566509cc06ce1baf7acd1277bdf1b5
matheusmarks <marquesmatheusoliveira01@gmail.com>
2022-08-14 11:15:10 -0300

    Project hr-auth created

--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -2,12 +2,8 @@ package com.devsuperior.hruser.resources;
 
 import com.devsuperior.hruser.entities.User;
 import com.devsuperior.hruser.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
